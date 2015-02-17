f76a0f57c7f552713436b9fe8a8dae3a6cb40cac
Chris Torstenson <c.torsday+github@gmail.com>
2015-02-17 14:47:06 -0800

    update routes for session control

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,10 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   
   get '/register' => 'users#new'
   post '/users' => 'users#create'

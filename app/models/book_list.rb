5c0600ee1e9fdcf9441ebd0605a741a0757dd884
Chris Torstenson <c.torsday+github@gmail.com>
2015-03-03 16:14:57 -0800

    add page_count method to book_list model

--- app/models/book_list.rb
+++ app/models/book_list.rb
@@ -2,4 +2,8 @@ class BookList < ActiveRecord::Base
   belongs_to :user
   has_many :books_book_lists
   has_many :books, :through => :books_book_lists
+
+  def page_count
+    self.books.inject(0){|sum, book| sum += book.page_count}
+  end
 end
